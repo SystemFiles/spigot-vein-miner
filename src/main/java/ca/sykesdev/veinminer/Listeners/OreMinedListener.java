3f9130ed366dd5624f71bf926c965b8964702dbc
Ben Sykes <ben@sykesdev.ca>
2020-05-10 13:34:17 -0400

    Added missing exp orb to block break event

--- src/main/java/ca/sykesdev/veinminer/Listeners/OreMinedListener.java
+++ src/main/java/ca/sykesdev/veinminer/Listeners/OreMinedListener.java
@@ -5,14 +5,16 @@ import ca.sykesdev.veinminer.VeinMiner;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class OreMinedListener implements Listener {
+
+    private final Map<Material, Integer> oreExpMap = new HashMap<>();
 
     // Instance of plugin
     private VeinMiner plugin;
@@ -23,6 +25,14 @@ public class OreMinedListener implements Listener {
      */
     public OreMinedListener(VeinMiner plugin) {
         this.plugin = plugin;
+
+        // Map all ores for exp drop amounts
+        oreExpMap.put(Material.COAL_ORE, 2);
+        oreExpMap.put(Material.REDSTONE_ORE, 5);
+        oreExpMap.put(Material.LAPIS_ORE, 5);
+        oreExpMap.put(Material.EMERALD_ORE, 7);
+        oreExpMap.put(Material.DIAMOND_ORE, 7);
+        oreExpMap.put(Material.NETHER_QUARTZ_ORE, 7);
     }
 
     /**
@@ -41,8 +51,15 @@ public class OreMinedListener implements Listener {
                 && event.getPlayer().getGameMode() == GameMode.SURVIVAL) {
 
             // Loop through all connected blocks and break them naturally
-            for (Block b : OreAPI.getAllConnected(broken)) {
+            Set<Block> connectedBlocks = OreAPI.getAllConnected(broken);
+            for (Block b : connectedBlocks) {
                 b.breakNaturally();
+                int expAmount = oreExpMap.get(b.getType());
+
+                // Spawn exp orb for number of blocks
+                ExperienceOrb orb = b.getWorld().spawn(b.getLocation()
+                        .add(0.5, 0.5, 0.5), ExperienceOrb.class);
+                orb.setExperience(expAmount * connectedBlocks.size());
             }
         }
     }
